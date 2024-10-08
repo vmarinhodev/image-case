940db6580dbd53c781432ff35522ded6288e5b26
Vitor Marinho <vitor@uw-s.nl>
2024-10-09 01:17:07 +0200

    some text added

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -5,6 +5,7 @@ export default function Photos() {
     return (
         <main className="min-h-screen">
             <div className="container mx-auto px-4 py-4">
+                <h1>This is the Photos page</h1>
                 <div className="flex flex-col items-center mb-6">
                     <h1 className="text-4xl font-bold mb-4">Photos</h1>
                     <PhotoUploader />
