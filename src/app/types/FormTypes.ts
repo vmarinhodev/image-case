6df678e43eb33da1361fa37acddd2b6e2a52ee52
Vitor Marinho <vitor@uw-s.nl>
2024-10-08 23:20:14 +0200

    folder deleted

--- src/app/types/FormTypes.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { loginSchema } from "../schemas/schemas";
-
-
-export type LoginFormFieldsProps = {
-    fieldName: keyof z.infer<typeof loginSchema>;
-    fieldType: 'text' | 'email' | 'password';
-    placeholder: string;
-};
