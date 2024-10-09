afcd75e9ceec9735a9af528437593dd9cc8d1005
Vitor Marinho <vitor@uw-s.nl>
2024-10-09 22:07:04 +0200

    deleted error page

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -2,19 +2,18 @@ import ImageGrid from "@/components/custom/ImageGrid";
 import PhotoUploader from "@/components/custom/PhotoUploader";
 
 export default function Photos() {
-    const showFavourites = false;
-    return (
-        <main className="min-h-screen">
-            <div className="container mx-auto px-4 py-4">
-                <h1>This is the Photos page</h1>
-                <div className="flex flex-col items-center mb-6">
-                    <h1 className="text-4xl font-bold mb-4">Photos</h1>
-                    <PhotoUploader />
-                </div>
-                <ImageGrid
-                    favourites={showFavourites}
-                />
-            </div>
-        </main>
-    )
+  return (
+    <main className="min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Photos</h1>
+      <div className="container mx-auto px-4 py-4">
+        <div className="flex flex-col items-center mb-6">
+          <PhotoUploader />
+        </div>
+        <ImageGrid
+          favourites={true}
+          showHearted={true}
+        />
+      </div>
+    </main>
+  )
 }
