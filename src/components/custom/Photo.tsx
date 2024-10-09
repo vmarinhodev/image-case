7d4bf8cb6e47e87ce8b4fec43630a84abfd7a40b
Vitor Marinho <vitor@uw-s.nl>
2024-10-09 10:57:28 +0200

    modal created

--- src/components/custom/Photo.tsx
+++ src/components/custom/Photo.tsx
@@ -1,7 +1,8 @@
-/* eslint no-use-before-define: 2 */  // --> ON
 'use client'
 
 import Image from "next/image"
+import { useState } from "react";
+import PhotoModal from "./PhotoModal";
 
 interface photoProps {
     src: string,
@@ -12,19 +13,28 @@ interface photoProps {
     // isFavourited: boolean
 }
 
-export default function Photo({src, alt, width, height}: Readonly<photoProps>){
+export default function Photo({ src, alt, width, height }: Readonly<photoProps>) {
+    const [showModal, setShowModal] = useState(false);
+
+    function toggleModal() {
+        setShowModal(!showModal)
+    }
     return (
-        <div
-            style={{width, height}}
-            className="relative w-auto h-auto shadow-md border border-white border-opacity-80 rounded-lg overflow-hidden cursor-pointer"
-        >
-            <Image
-                src={src}
-                alt={alt}
-                fill
-                style={{objectFit: 'cover', objectPosition: 'center'}}
-                loading="lazy"
-            />
-        </div>
-    )
-}
+        <>
+            <div
+                style={{ width, height }}
+                className="relative w-auto h-auto shadow-md border border-white border-opacity-80 rounded-lg overflow-hidden cursor-pointer"
+            >
+                <Image
+                    src={src}
+                    alt={alt}
+                    fill
+                    style={{ objectFit: 'cover', objectPosition: 'center' }}
+                    loading="lazy"
+                    onClick={() => setShowModal(true)}
+                />
+                {showModal && <PhotoModal src={src} alt={alt} onClose={toggleModal} />}
+            </div>
+        </>
+    );
+};
