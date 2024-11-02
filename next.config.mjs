fe0c3daabaf40f823f6bb483f0a251285f13fc9c
Vitor Marinho <vitor@uw-s.nl>
2024-11-02 01:02:18 +0100

    conflict solved and saved

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-<<<<<<< HEAD
-                protocol: 'https',
-                hostname: 'jdltlsfunkwytxckzmhg.supabase.co',
-=======
                 protocol: 'http',
                 // hostname: 'jdltlsfunkwytxckzmhg.supabase.co',
                 hostname: '127.0.0.1',
->>>>>>> personal-page
             }
         ]
     },
